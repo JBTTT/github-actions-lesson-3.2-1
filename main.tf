4608cb65515e3ee218c94de69fe4909c0327cdb2
jibin <perseverancejbt@hotmail.com>
2025-10-22 21:22:49 +0800

    Fixing fmt error

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
-    key    = "jibin-s3-tf-ci.tfstate"  #Change this
+    key    = "jibin-s3-tf-ci.tfstate" #Change this
     region = "us-east-1"
   }
 }
@@ -14,7 +14,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
   name_prefix = jibin.tan #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {

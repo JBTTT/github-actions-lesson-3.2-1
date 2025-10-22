310d9d5b29ce463938b4964e489230ca39b099a6
jibin <perseverancejbt@hotmail.com>
2025-10-22 21:41:15 +0800

    Fixing Terraform provider issue

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.13.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
     key    = "jibin-s3-tf-ci.tfstate" #Change this
